00e07bd248d7724b48affb41cd71f683f090f5da
Jaydev A. Bapat <70470113+jaydevbapat@users.noreply.github.com>
2025-08-16 13:38:06 +0530

    Update Memory_fifo.v

--- Memory_fifo.v
+++ Memory_fifo.v
@@ -1,13 +1,13 @@
 module Memory_fifo(
 			output 	[15:0] 	read_data,
 			input 	[15:0] 	write_data,
-			input 	[5:0] 	write_address, read_address,
+			input 	[6:0] 	write_address, read_address,
 			input 				write_enable, full, clock_write);
 
 	
 	
-	// memory with 16 bit data and 64 locations(depth)
-	reg [15:0] mem [0:63];
+	// memory with 16 bit data and 127s locations(depth)
+	reg [15:0] mem [0:127];
 	
 	assign read_data = mem [read_address];
 	
